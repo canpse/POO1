96f5b2715914db7265841b66d78fea83a77ce545
canpse <74630551+canpse@users.noreply.github.com>
2021-01-27 13:03:28 -0300

    Add files via upload

---
+++ selecao/src/selecao/Ativdade_selecao.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ativdade_selecao {
+
+	public static void main(String[] args) {
+		
+//		Scanner entrada = new Scanner(System.in);
+//		
+//		System.out.print("Qual exercicio voce quer ver: ");
+//		int num = entrada.nextInt();
+//				
+//		switch (num) {
+//		case 1:
+//			System.out.println("Exercicio 2 - selecionado");
+//			System.out.print("\nInforme seu peso");
+//			double peso = entrada.nextDouble();
+//			System.out.print("\nInforme sua altura");
+//			System.out.println("Qual seu genero? m (masculino) ou f (feminino)");
+//			
+//			String genero = "";
+//			genero = entrada.nextLine();
+//			break;
+			
+		}
+
+	}
+
+}
